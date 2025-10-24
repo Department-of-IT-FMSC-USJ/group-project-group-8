f8cee3119208f0e444692b5e98a9c12c772a534e
Ruvin Palihawadana <167301866+Ruvinology@users.noreply.github.com>
2025-10-24 23:23:42 +0530

    Change database port from 3308 to 3306

--- config/database.php
+++ config/database.php
@@ -6,7 +6,7 @@ define('DB_HOST', 'localhost');
 define('DB_NAME', 'debit_card_renewal');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_PORT', '3308');
+define('DB_PORT', '3306');
 
 
 $conn = null;
